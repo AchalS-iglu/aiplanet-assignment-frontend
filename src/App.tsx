04d1b037fa45f5ad2b1d3576aaffd7246f903239
Achal Singhal <achals2002@gmail.com>
2024-03-22 07:45:24 +0530

    typing fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useContext, useEffect } from "react";
 import "./App.css";
 import MessageWindow from "./components/messageWindow";
 import NavBar from "./components/navbar";
-import { getFilesList } from "./lib/api-wrapper";
-import { toast } from "sonner";
-import { FileContext } from "./lib/file-context";
 
 function App() {
-    const { setFileList } = useContext(FileContext);
-
     return (
         <div className="flex flex-col h-screen">
             <NavBar />
